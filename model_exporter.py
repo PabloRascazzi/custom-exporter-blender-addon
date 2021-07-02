3621fa8eb5ed3960b0808e2d8126da0d5bc4d556
Pablo Rascazzi <71659490+PabloRascazzi@users.noreply.github.com>
2021-07-02 18:11:08 -0400

    exporter version 0.2

--- model_exporter.py
+++ model_exporter.py
@@ -6,21 +6,22 @@ bl_info = {
     "name": "Model Exporter Add-on",
     "description": "Exports model data with vertices and indices in a proper format for OpenGL.",
     "author": "Pablo Rascazzi",
-    "version": (0, 1),
+    "version": (0, 2),
     "blender": (2, 92, 0),
     "location": "File > Export > Model Exporter (.mdl)",
     "category": "Import-Export"
 }
 
-logging = False
+class Error(Exception):
+    pass
 
-def process_model_data(object, model_type):
+def process_model_data(object, model_type, logging):
     if logging == True: print("Processing object data...")
     
     if model_type == 'xyz': vertices_stride = 3
     elif model_type == 'xyzuv': vertices_stride = 5
     elif model_type == 'xyzuvtbn': vertices_stride = 8
-    else: sys.exit('Error: Invalid model type.')
+    else: raise Error('Invalid model type.')
     
     mesh = object.data
     loop = mesh.loops
@@ -83,60 +84,97 @@ def process_model_data(object, model_type):
     
     return vertices_stride, vertices_size, vertices, indices_size, indices
 
-def write_model_data(context, filepath, model_type, file_format, separate_object):
-    vertices_stride, vertices_size, vertices, indices_size, indices = process_model_data(bpy.context.object, model_type)
-
-    if logging == True: # Logs for debugging
-        print('Exporter Version: %d.%d' % (bl_info['version'][0], bl_info['version'][1]))
-        print('Vertices Stride: %d' % vertices_stride)
-        print('Vertices Size: %d' % vertices_size)
-        print('Vertices Array:', vertices)
-        print('Material Count: %d' % indices_size)
-        for i in range(indices_size):
-            print('Indices[%d] Size:' % i,len(indices[i]))
-            print('Indices[%d]:' % i, indices[i])
+def make_objects_list(selection, logging):
+    objects_list = []
+    if logging == True: print('Fetching objects list...')
     
-    if file_format == 'Binary':
-        if logging == True: print("Writing binary data to file...")
-        f = open(filepath, 'wb')
-        
-        f.write(bl_info['version'][0].to_bytes(4, sys.byteorder, signed=True))
-        f.write(bl_info['version'][1].to_bytes(4, sys.byteorder, signed=True))
-        f.write(vertices_stride.to_bytes(4, sys.byteorder, signed=True))
-        f.write(vertices_size.to_bytes(4, sys.byteorder, signed=True))
-        vertices_byte_array = numpy.array(vertices, 'float32')
-        vertices_byte_array.tofile(f)
-        f.write(indices_size.to_bytes(4, sys.byteorder, signed=True))
-        for i in range(indices_size):
-            f.write(len(indices[i]).to_bytes(4, sys.byteorder, signed=True))
-            indices_byte_array = numpy.array(indices[i], 'int_')
-            indices_byte_array.tofile(f)
-        
-        f.close()
-    elif file_format == 'ASCII':
-        if logging == True: print("Writing ASCII data to file...")
-        f = open(filepath, 'w')
-        
-        f.write('vers %d %d\n' % (bl_info['version'][0], bl_info['version'][1]))
-        f.write('vstr %d\n' % vertices_stride)
-        f.write('vsiz %d\n' % vertices_size)
-        f.write('varr')
-        for vert in vertices:
-            f.write(' %f' % vert)
-        f.write('\n')
-        f.write('matc %d\n' % indices_size)
-        for i in range(indices_size):
-            f.write('isiz %d\n' % len(indices[i]))
-            f.write('iarr')
-            for indx in indices[i]:
-                f.write(' %d' % indx)
-            f.write('\n')
-        
-        f.close()
-        
-    else:
-        sys.exit('Error: Invalid file format.')
+    if selection == 'selected_object':
+        object = bpy.context.object
+        if object != None and object.type == 'MESH': 
+            objects_list.append(object)
+    elif selection == 'all_objects':
+        for object in bpy.context.visible_objects:
+            if object != None and object.type == 'MESH': 
+                objects_list.append(object)
+    else: raise Error('Invalid selection.')
+    
+    if len(objects_list) == 0: raise Error('Object selection(s) have no mesh to export.')
+    if logging == True: print('Object List:', objects_list)
+    
+    return objects_list
 
+def write_model_data(file_path, file_ext, model_type, selection, file_format, logging):
+    objects_list = make_objects_list(selection, logging)
+    
+    for object in objects_list:
+        # Fetch and process object's data
+        vertices_stride, vertices_size, vertices, indices_size, indices = process_model_data(object, model_type, logging)
+
+        # Logs for debugging
+        if logging == True: 
+            print('Exporter Version: %d.%d' % (bl_info['version'][0], bl_info['version'][1]))
+            print('Vertices Stride: %d' % vertices_stride)
+            print('Vertices Size: %d' % vertices_size)
+            print('Vertices Array:', vertices)
+            print('Material Count: %d' % indices_size)
+            for i in range(indices_size):
+                print('Indices[%d] Size:' % i,len(indices[i]))
+                print('Indices[%d]:' % i, indices[i])
+        
+        # Find correct filepath
+        if len(objects_list) == 1: filepath = file_path
+        else: filepath = file_path.replace(file_ext, '_' + object.name + file_ext)
+        if logging == True: print('File save location: %s' % filepath)
+        
+        # Write data to file
+        if file_format == 'Binary':
+            if logging == True: print("Writing binary data to file...")
+            f = open(filepath, 'wb')
+            
+            f.write(bl_info['version'][0].to_bytes(4, sys.byteorder, signed=True))
+            f.write(bl_info['version'][1].to_bytes(4, sys.byteorder, signed=True))
+            f.write(vertices_stride.to_bytes(4, sys.byteorder, signed=True))
+            f.write(vertices_size.to_bytes(4, sys.byteorder, signed=True))
+            vertices_byte_array = numpy.array(vertices, 'float32')
+            vertices_byte_array.tofile(f)
+            f.write(indices_size.to_bytes(4, sys.byteorder, signed=True))
+            for i in range(indices_size):
+                f.write(len(indices[i]).to_bytes(4, sys.byteorder, signed=True))
+                indices_byte_array = numpy.array(indices[i], 'int_')
+                indices_byte_array.tofile(f)
+            
+            f.close()
+        elif file_format == 'ASCII':
+            if logging == True: print("Writing ASCII data to file...")
+            f = open(filepath, 'w')
+            
+            f.write('vers %d %d\n' % (bl_info['version'][0], bl_info['version'][1]))
+            f.write('vstr %d\n' % vertices_stride)
+            f.write('vsiz %d\n' % vertices_size)
+            f.write('varr')
+            for vert in vertices: f.write(' %f' % vert)
+            f.write('\n')
+            f.write('matc %d\n' % indices_size)
+            for i in range(indices_size):
+                f.write('isiz %d\n' % len(indices[i]))
+                f.write('iarr')
+                for indx in indices[i]: f.write(' %d' % indx)
+                f.write('\n')
+            
+            f.close()      
+        else: raise Error('Invalid file format.')
+        if logging == True: print('Finished exporting %s...' % object.name)
+    return
+
+
+def export_model(context, filepath, filename_ext, type, selection, format, log_setting):
+    print('\nMODEL EXPORT STARTED')
+    
+    try:
+        write_model_data(filepath, filename_ext, type, selection, format, log_setting)
+    except Error as e: 
+        print('Error:', e)
+        
     print('MODEL EXPORT FINISHED')
     return {'FINISHED'}
 
@@ -157,7 +195,7 @@ class ExportModel(Operator, ExportHelper):
     filename_ext = ".mdl"
 
     filter_glob: StringProperty(
-        default="*.model",
+        default="*.mdl",
         options={'HIDDEN'},
         maxlen=255,  # Max internal buffer length, longer would be clamped.
     )
@@ -175,6 +213,16 @@ class ExportModel(Operator, ExportHelper):
         default='xyzuvtbn',
     )
     
+    selection: EnumProperty(
+        name="Selection",
+        description="Select which objects to export",
+        items=(
+            ('selected_object', "Selected Object", "Export currently selected object only"),
+            ('all_objects', "All Objects", "Export all objects"),
+        ),
+        default='selected_object',
+    )
+    
     format: EnumProperty(
         name="Format",
         description="Choose file format",
@@ -185,15 +233,15 @@ class ExportModel(Operator, ExportHelper):
         default='Binary',
     )
     
-    use_setting: BoolProperty(
-        name="Separate object",
-        description="Separate all objects into different files",
+    log_setting: BoolProperty(
+        name="Log to console",
+        description="Logs all data and calls to console",
         default=True,
     )
 
     def execute(self, context):
-        return write_model_data(context, self.filepath, self.type, self.format, self.use_setting)
-
+        return export_model(context, self.filepath, self.filename_ext, self.type, self.selection, self.format, self.log_setting)
+    
 
 # Only needed if you want to add into a dynamic menu
 def menu_func_export(self, context):
@@ -215,3 +263,4 @@ if __name__ == "__main__":
 
     # test call
     bpy.ops.export_test.some_data('INVOKE_DEFAULT')
+    
